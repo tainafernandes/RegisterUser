225d41978eea3acd59e5c15ae744d2fbd6f9f78c
tainafernandes <tainafefe@gmail.com>
2023-06-23 15:47:29 -0300

    Adding typeorm dependency

--- backend/register-user/src/user/user.ts
+++ backend/register-user/src/user/user.ts
@@ -1,4 +1,15 @@
+import {Entity, Column, PrimaryGeneratedColumn} from 'typeorm'
+
+
+@Entity()
 export class User {
 
-    private name: String;
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    password: string;
 }
